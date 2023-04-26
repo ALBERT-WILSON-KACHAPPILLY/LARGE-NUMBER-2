f6516d8df51609ed4ea9980e5a435bd512c32048
WOLVERINE0000 <128771086+WOLVERINE0000@users.noreply.github.com>
2023-04-26 23:59:31 +0530

    Add files via upload

---
+++ AC2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+   int a,b,c,l;
+   printf("Enter three numbers\n"); 
+   scanf("%d%d”,&a,&b, &c);
+if(a>b) 
+{ if(a>c)
+{printf("larger number is & d",a);
+l=a; 
+}
+} 
+else
+{if(b>c) {
+printf("Larger number is %d",b) ;
+l=b
+} else
+{printf("larger number is %d",c);
+l=c
+}} 
+   printf(" The large number is %d ”, l); 
+return 0;
+}
